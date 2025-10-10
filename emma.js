df69c8617eac20e7c98340fc5320490af2a20aaa
tobi580 <tobi58022@gmail.com>
2025-10-10 13:01:05 +0100

    bday

---
+++ emma.js
@@ -0,0 +1,8 @@
+        function myfunction(){
+           const mywindow = window.open();
+           mywindow.document.write("<h1>Happy Birthday Bro</h1>")
+           mywindow.document.write("<h3><strong>To my incredible elder brother🎉🎊, Happy Birthday🎂! Thank you for being my rock, my confidant, and my biggest supporter. Our sibling bond is something truly special🍀, and I'm so lucky✨ to have you in my life. You've taught me so much, and I can't wait to see what amazing things you'll accomplish this year🎁. Wishing you a birthday filled with all the happiness🥳, love🥰, and laughter you deserve, and here's to many more unforgettable moments together🍻. </strong></h3>")
+           mywindow.document.write(" <button onclick="/">OK</button>")
+           
+          
+        }
